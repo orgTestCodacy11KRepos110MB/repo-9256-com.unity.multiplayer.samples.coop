84f3fe34feaed664fefc30202808dd4e98aedbd4
Samuel Bellomo <samuel.bellomo@unity3d.com>
2022-12-12 09:05:48 -0500

    fixing issue with 2022 version bump

--- Assets/Scripts/Utils/EnableOrDisableColliderOnAwake.cs
+++ Assets/Scripts/Utils/EnableOrDisableColliderOnAwake.cs
@@ -12,7 +12,10 @@ namespace Unity.BossRoom.Utils
 
         void Awake()
         {
-            m_Collider.enabled = m_EnableStateOnAwake;
+            if (m_Collider != null) // tmp, do not commit!!!!!!
+            {
+                m_Collider.enabled = m_EnableStateOnAwake;
+            }
         }
     }
 }
